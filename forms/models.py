e294ccfa896d56a3a2033c6c5c09bea607d27ef2
Clay Beabout <beaboutclayton@gmail.com>
2023-03-26 08:09:05 -0500

    forms: models: add __str__ methods to Product, Order, ProductOrder

--- forms/models.py
+++ forms/models.py
@@ -4,6 +4,9 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
 
+    def __str__(self):
+      return self.name
+
 class Order(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
@@ -11,6 +14,12 @@ class Order(models.Model):
     order_date = models.DateTimeField('Order Date')
     order_total = models.IntegerField()
 
+    def __str__(self):
+       return f'{self.name} {self.order_date.strftime("(%m-%d-%Y @ %H:%M:%S)")}'
+
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    
+    def __str__(self):
+       return f'{self.order}, {self.product}'
