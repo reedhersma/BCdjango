a946fb49faf888eaec207e29c45a123a1cbfe995
Clay Beabout <beaboutclayton@gmail.com>
2023-03-25 23:08:27 -0500

    forms: models: remove incorrect space in class definitions

--- forms/models.py
+++ forms/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-class Product (models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
 
-class Order (models.Model):
+class Order(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=10)
     order_date = models.DateTimeField('Order Date')
     order_total = models.IntegerField()
 
-class ProductOrder (models.Model):
+class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
