7751de01ffa3fa9c5aee8117c926e7b3b16a3962
reedhersma <reed.hersma@yahoo.com>
2023-03-25 21:47:15 -0500

    Settings Update

--- BandanaCoffee/settings.py
+++ BandanaCoffee/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a47nc_o-ta2i=8k44!(5ftmot(7%g9(yz+mo7v10#1q58zvdg_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forms.apps.FormsConfig'
 ]
 
 MIDDLEWARE = [
