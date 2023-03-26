fc2af3a8fcbabbb5c7e4bb9a1d2790dc961a0ec6
Clay Beabout <beaboutclayton@gmail.com>
2023-03-25 22:10:29 -0500

    forms: models: remove product from Order class

--- forms/models.py
+++ forms/models.py
@@ -8,8 +8,7 @@ class Order (models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=10)
-    product = models.TextChoices(Product.name)
-    Order_date = models.DateTimeField('Order Date')
+    order_date = models.DateTimeField('Order Date')
     order_total = models.IntegerField()
 
 class ProductOrder (models.Model):
