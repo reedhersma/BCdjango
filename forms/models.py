4a6eeb5db336f28213022ae460484c94e9b1fb36
reedhersma <reed.hersma@yahoo.com>
2023-03-25 21:19:16 -0500

    Forms App Update 3.25.23

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Product (models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+class Order (models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    product = models.TextChoices(Product.name)
+    Order_date = models.DateTimeField('Order Date')
+    order_total = models.IntegerField()
+
+class ProductOrder (models.Model):
+    product = models.Charfield(Product.name)
+    client = models.CharField(Order.name)
+    email = models.CharField(Order.email)
+    phone = models.CharField(Order.phone)
+    Order_date = models.CharField(Order.Order_date)
+    order_total = models.CharField(Order.order_total)
